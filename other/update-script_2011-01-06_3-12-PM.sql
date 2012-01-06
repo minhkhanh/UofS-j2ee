3fc8e1852b25ec0e0859e9c6f457bded31207e33
akhoi90 <akhoi90@9f546699-c5bb-550f-9b22-400a2577564b>
2012-01-06 08:14:32 +0000

    update ChiTietDauGia table

---
+++ other/update-script_2011-01-06_3-12-PM.sql
@@ -0,0 +1,18 @@
+﻿drop table `daugia`.`chitietdaugia`
+
+delimiter $$
+
+CREATE TABLE `chitietdaugia` (
+  `MaChiTietDauGia` int(11) NOT NULL AUTO_INCREMENT,
+  `MaTaiKhoan` int(11) NOT NULL,
+  `MaSanPham` int(11) NOT NULL,
+  `GiaGiaoDich` int(11) DEFAULT NULL,
+  `ThoiGianGiaoDich` datetime DEFAULT NULL,
+  `TinhTrang` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`MaChiTietDauGia`,`MaTaiKhoan`,`MaSanPham`),
+  KEY `FK_CHITIETDAUGIA_SANPHAM` (`MaSanPham`),
+  KEY `FK_CHITIETDAUGIA_TAIKHOAN` (`MaTaiKhoan`),
+  CONSTRAINT `FK_CHITIETDAUGIA_SANPHAM` FOREIGN KEY (`MaSanPham`) REFERENCES `sanpham` (`MaSanPham`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_CHITIETDAUGIA_TAIKHOAN` FOREIGN KEY (`MaTaiKhoan`) REFERENCES `taikhoan` (`MaTaiKhoan`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci$$
+
